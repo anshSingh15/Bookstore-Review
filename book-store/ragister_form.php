859d6e12eccd2e8af7067f5b9e582f28cae38e2e
Ansh Singh <ansh23004@gmail.com>
2025-06-16 10:08:56 +0530

    Initial commit

---
+++ book-store/ragister_form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="login-form-container">
+      <div id="close-login-btn" class="fas fa-times"></div>
+
+      <form action="">
+        <h3>Registor Now</h3>
+        <span>eter your name</span>
+        <input type="name" name="" class="box" placeholder="enter your name" id=""/>
+
+        <span>eter your email</span>
+        <input type="email" name="" class="box" placeholder="enter your email" id="" />
+         
+        <span>eter your password</span>
+        <input type="password" name="" class="box" placeholder="enter your password" id="" />
+
+        <span>confirm your password</span>
+        <input type="password" name="" class="box" placeholder="enter your password" id="" />
+
+       
+        <input type="submit" value="Registor Now" class="btn" />
+        <p>already have an account? <a href="#">logi now!</a></p>
+      </form> 
+    </div> 
+</body>
+</html>
